f33e3bbc1d2dbdf4d61f34a46020710e1061cc4f
Andrii Drozdenko <adrozdenko@gmail.com>
2020-10-17 00:38:53 +0300

    Mongo db

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true, useUnifiedTopology: true }).catch(function (reason) {
+    console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.error('error'));
+db.once('open', () => console.log('Connected to Database'));
 
 app.listen(3000, () => {
     console.log('Server Started')
